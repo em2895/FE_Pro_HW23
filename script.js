b2711003b2eecc9a108e889de6d7f17e64537ff8
Emil Akhmedov <emil282@ukr.net>
2023-04-09 20:05:31 +0300

    add index.html, script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+//Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)
+
+const contentTable = document.getElementById('contentTable');
+const table = document.createElement('table');
+let counter = 1;
+
+contentTable.appendChild(table);
+table.setAttribute("style", "border: 2px red solid;" )
+
+for (let i = 0; i < 10; i++) {
+    const tr = document.createElement('tr');
+    table.appendChild(tr);
+    for (let j = 0; j < 10; j++) {
+        const td = document.createElement('td');
+        tr.appendChild(td);
+        td.innerHTML += counter;
+        counter++;
+    }
+}
